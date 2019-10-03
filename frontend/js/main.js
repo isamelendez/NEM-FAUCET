c697453182ecac7b138d603837a4d99ff65bc402
isamelendez <isabel.melendez1997@gmail.com>
2019-10-03 13:26:47 -0600

    app

---
+++ frontend/js/main.js
@@ -0,0 +1,64 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+  
+    /*==================================================================
+    [ Validate ]*/
+    var to = $('.validate-input input[name="to"]');
+    var amount = $('.validate-input input[name="amount"]');
+    var email = $('.validate-input input[name="email"]');
+    var message = $('.validate-input textarea[name="message"]');
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(to).val().trim() == ''){
+            showValidate(to);
+            check=false;
+            return check;
+        }
+
+        else {
+           window.location.reload();
+        }
+
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,51 @@
 var express = require('express');
 var router = express.Router();
-let mysql = require('mysql');
-
-let connection = mysql.createConnection({
-  // host     : 'goku.cwzshpcfjbsy.us-east-1.rds.amazonaws.com',
-  // password : 'Isabelgoku', //'AdminCloud',
-  host     : 'cloudrds.cvvtkb9yuaiz.us-east-2.rds.amazonaws.com',
-  password : 'AdminCloud',
-  user     : 'admin',
-  database : 'configuration'
-});
-
-connection.connect(function(err) {
-  if (err) throw err
-  console.log('connection successful')
-})
-
+var nemapi = require('nem-api');
+var nem = require("nem-sdk").default;
 
 router.get('/', function(req, res, next) {
-  res.status(200).send("working")
+  res.send("working");
 });
 
-router.get('/ads', function(req, res, next) {
-  const campaigns = req.query.advertiser_campaigns
-  connection.query(`SELECT DISTINCT campaign_id, id, headline, description, url FROM ads JOIN campaign_ads ON ads.id = campaign_ads.ad_id WHERE campaign_ads.campaign_id IN (${campaigns}) GROUP BY find_in_set(campaign_id, '${campaigns}');`, function (error, results, fields) {
-    if (error) throw error;
-    res.status(200).send( results );
+
+router.get('/transaction', function(req, res, next) {
+  let to = req.query.to;
+  let amount = req.query.amount;
+  let message = req.query.message;
+  let faucetOwnerPassword = ''; //Your password here
+  let faucetOwnerPrivateKey = ''; //Your private key here
+
+  let endpoint = nem.model.objects.create('endpoint')(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
+  let common = nem.model.objects.create('common')(faucetOwnerPassword, faucetOwnerPrivateKey);
+  let transferTransaction = nem.model.objects.create('transferTransaction')(to, amount, message);
+  let preparedTransaction = nem.model.transactions.prepare('transferTransaction')(common, transferTransaction, nem.model.network.data.testnet.id);
+  nem.com.requests.chain.time(endpoint).then(function (timeStamp) {
+    const ts = Math.floor(timeStamp.receiveTimeStamp / 1000);
+    preparedTransaction.timeStamp = ts;
+    const due = 60;
+    preparedTransaction.deadline = ts + due * 60;
+   
+    console.log(preparedTransaction);
+   
+    nem.model.transactions.send(common, preparedTransaction, endpoint).then(function(res){
+        console.log(res);
+    }, function(err){
+        console.log(err);
     });
-         
+
+  }, function (err) {
+      console.error(err);
+  });
 });
+
+// router.get('/ads', function(req, res, next) {
+//   const campaigns = req.query.advertiser_campaigns
+//   connection.query(`SELECT DISTINCT campaign_id, id, headline, description, url FROM ads JOIN campaign_ads ON ads.id = campaign_ads.ad_id WHERE campaign_ads.campaign_id IN (${campaigns}) GROUP BY find_in_set(campaign_id, '${campaigns}');`, function (error, results, fields) {
+//     if (error) throw error;
+//     res.status(200).send( results );
+//     });
+         
+// });
 
 
 module.exports = router;
